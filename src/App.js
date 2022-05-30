1c5fc4cf480459b503af6a1a139448bc34579348
Gabriel Moraes <gmoraes.mat@gmail.com>
2022-05-30 07:48:45 -0300

    implementação do App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import TokenContext from "../contexts/tokenContext";
+import Cadastro from ".components/Cadastro";
+import Login from ".components/Login";
+import Hoje from ".components/Hoje";
+import Historico from ".components/Historico";
+import Habitos from ".components/Habitos";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+export default function App(){
+
+    const [token, setToken] = useState("");
+    const [usuarioLogado, setUsuaruiLogado] = useState("");
+    const [porcentagemConcluidos, setPorcentagemConcluidos] = useState(0);
+
+    
+
+  return(
+            <BrowserRouter>
+            <TokenContext.Provider value = {{token, setToken, usuarioLogado, setUsuaruiLogado, porcentagemConcluidos, setPorcentagemConcluidos}}>
+                <Routes>
+                    <Route path="/" element={<Login/>} />
+                    <Route path="/cadastro" element={<Cadastro/>} />
+                    <Route path="/hisotrico" element={<Historico />} />
+                    <Route path="/habitos" element={<Habitos />} />
+                    <Route path="/hoje" element={<Hoje />} />
+                </Routes>
+            </TokenContext.Provider>    
+            </BrowserRouter>
+
   );
 }
-
-export default App;
---
+++ src/components/Cadastro.js
@@ -0,0 +1,10 @@
+import react from "react";
+import { Link } from "react-router-dom";
+import styledComponents from "styled-components";
+ 
+export default function Cadastro(){
+
+    return(
+            
+    )
+}
